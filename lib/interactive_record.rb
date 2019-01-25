2565323fbc41731fa844793a1246c253adb267f2
Ryan Brady <ryanbrady369@gmail.com>
2019-01-25 00:23:49 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -62,7 +62,7 @@ class InteractiveRecord
     col = column.keys[0].to_s
     value = column.values[0]
 
-    sql = "SELECT * FROM #{self.table_name} WHERE #{col} = #{value}"
+    sql = "SELECT * FROM #{table_name} WHERE #{col} = #{value}"
 
     DB[:conn].execute(sql)
   end
